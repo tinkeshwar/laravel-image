836f354077db92117424d4e11db5e631b2dcf210
Tinkeshwar <tinkeshwar@gmail.com>
2022-05-24 22:29:45 +0530

    2.3.10

--- database/migrations/2022_05_24_145220_add_ratio_to_images_table.php
+++ database/migrations/2022_05_24_145220_add_ratio_to_images_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddDriverToImagesTable extends Migration
+class AddRatioToImagesTable extends Migration
 {
     /**
      * Run the migrations.
